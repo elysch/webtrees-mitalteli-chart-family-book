f418fd1f2d64bb00cb9fda69234277a970d5d76a
elysch <ely.github.1@mitalteli.com>
2025-04-25 20:55:04 -0600

    Added translation to Spanish

---
+++ resources/lang/es.php
@@ -0,0 +1,35 @@
+<?php
+
+    return [
+
+    "Enhanced family book" => 'Libro familiar mejorado', // FamilyBookChartEnhancedModule.php - function title()
+    "A chart of an individual’s ancestors and descendants, as a family book (enhanced with marriages, images, and more)." => 'Un gráfico de los antepasados y descendientes de un individuo, como un libro familiar (mejorado con matrimonios, imágenes y más).', // FamilyBookChartEnhancedModule.php - function description()
+    "Enhanced family book of %s" => 'Libro familiar mejorado de %s', // FamilyBookChartEnhancedModule.php - function chartTitle()
+    "Family with spouse" => 'Familia con esposa', // resources\views\chart-box.phtml.php
+    "Links" => 'Ligas', // resources\views\chart-box.phtml.php and resources\views\chart-box-marriage.phtml
+    "Family of %s" => 'Familia de %s', // resources\views\modules\mitalteli-family-book-chart\chart.phtml
+    #"Individual" => 'Persona', // resources\views\modules\mitalteli-family-book-chart\page.phtml
+    "Generations" => 'Generaciones', // resources\views\modules\mitalteli-family-book-chart\page.phtml
+    "Descendant generations" => 'Generaciones de descendientes', // resources\views\modules\mitalteli-family-book-chart\page.phtml
+    #"Spouses" => 'Esposas', // resources\views\modules\mitalteli-family-book-chart\page.phtml
+    "Show spouses" => 'Mostrar esposas', // resources\views\modules\mitalteli-family-book-chart\page.phtml
+    #"Marriages" => 'Matrimonios', // resources\views\modules\mitalteli-family-book-chart\page.phtml
+    "Show marriages" => 'Mostrar matrimonios', // resources\views\modules\mitalteli-family-book-chart\page.phtml
+    "Show full places" => 'Mostrar lugares completos', // resources\views\modules\mitalteli-family-book-chart\page.phtml
+    "Extra images" => 'Imágenes adicionales', // resources\views\modules\mitalteli-family-book-chart\page.phtml
+    "Show extra images" => 'Mostrar imágenes adicionales', // resources\views\modules\mitalteli-family-book-chart\page.phtml
+    "To generate a pdf you can use print function and select &quot;Save as PDF&quot;, then specify a scale percentage in &quot;more options&quot;. With Chrome and Edge browsers on Windows OS, 47&percnt; has worked fine for me when showing 5 generations horizontaly and setting landscape US Letter page." => 'Para generar un PDF, puede usar la función de impresión y seleccionar "Guardar como PDF". Luego, especifique un porcentaje de escala en "Más opciones". Con los navegadores Chrome y Edge en Windows, el 47&percent; me ha funcionado correctamente al mostrar 5 generaciones horizontalmente y configurar la página en formato Carta horizontal.', // resources\views\modules\mitalteli-family-book-chart\page.phtml
+    "view" => 'Ver', // resources\views\modules\mitalteli-family-book-chart\page.phtml
+    "print" => 'Imprimir', // resources\views\modules\mitalteli-family-book-chart\page.phtml
+    "html" => 'html', // resources\views\modules\mitalteli-family-book-chart\page.phtml
+    "pdf" => 'pdf', // resources\views\modules\mitalteli-family-book-chart\page.phtml
+    "A3 (297mm x 420mm)" => 'A3 (297mm x 420mm)', // resources\views\modules\mitalteli-family-book-chart\page.phtml
+    "A4 (210mm x 297mm)" => 'A4 (210mm x 297mm)', // resources\views\modules\mitalteli-family-book-chart\page.phtml
+    "LETTER (8.5\" x 11\")" => 'Carta (8.5" x 11")', // resources\views\modules\mitalteli-family-book-chart\page.phtml
+    "LEGAL (8.5\" x 14\")" => 'Oficio (8.5" x 14")', // resources\views\modules\mitalteli-family-book-chart\page.phtml
+    "portrait" => 'vertical', // resources\views\modules\mitalteli-family-book-chart\page.phtml
+    "landscape" => 'horizontal', // resources\views\modules\mitalteli-family-book-chart\page.phtml
+    "download" => 'descargar', // resources\views\modules\mitalteli-family-book-chart\page.phtml
+
+    ];
+?>
