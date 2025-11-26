275f859a0c257d3244caf0a6c83514026db474bb
elysch <ely.github.1@mitalteli.com>
2025-11-26 17:55:43 -0600

    ca Translation

---
+++ resources/lang/ca.php
@@ -0,0 +1,45 @@
+<?php
+
+    return [
+
+    "Links" => 'Enlla&#xE7;os', // resources\views\chart-box.phtml.php and resources\views\chart-box-marriage.phtml
+    "Family with spouse" => 'Fam&#xED;lia amb esposa', // resources\views\chart-box.phtml.php
+    "Family of %s" => 'Fam&#xED;lia de %s', // resources\views\modules\mitalteli-family-book-chart\chart.phtml
+    #"Individual" => 'Persona', // resources\views\modules\mitalteli-family-book-chart\page.phtml - Already included in webtrees translations
+    "Generations" => 'Generacions', // resources\views\modules\mitalteli-family-book-chart\page.phtml
+    "Descendant generations" => 'Generacions de descendients', // resources\views\modules\mitalteli-family-book-chart\page.phtml
+    "Places format" => 'Format de lloc', // resources\views\modules\mitalteli-family-book-chart\page.phtml
+    #"Spouses" => 'Esposes', // resources\views\modules\mitalteli-family-book-chart\page.phtml - Already included in webtrees translations
+    "Show spouses" => 'Mostrar esposes', // resources\views\modules\mitalteli-family-book-chart\page.phtml
+    #"Marriages" => 'Matrimonis', // resources\views\modules\mitalteli-family-book-chart\page.phtml - Already included in webtrees translations
+    "Show marriages" => 'Mostrar matrimonis', // resources\views\modules\mitalteli-family-book-chart\page.phtml
+    "Divorces" => 'Divorcis', // resources\views\modules\mitalteli-family-book-chart\page.phtml
+    "Show divorces" => 'Mostrar divorcis', // resources\views\modules\mitalteli-family-book-chart\page.phtml
+    "Extra images" => 'Imatges addicionals', // resources\views\modules\mitalteli-family-book-chart\page.phtml
+    "Show extra images" => 'Mostrar imatges addicionals', // resources\views\modules\mitalteli-family-book-chart\page.phtml
+    'Debug level' => 'Nivell de depuraci&#xF3; (debug)', // modules/mitalteli-family-book-chart/page.phtml
+    'No debug' => 'Sense cap depuraci&#xF3; (debug)', // modules/mitalteli-family-book-chart/page.phtml
+    'Place substitution debug' => 'Depuraci&#xF3; de substituci&#xF3; de lloc (depura)', // modules/mitalteli-family-book-chart/page.phtml
+    "view" => 'veure', // resources\views\modules\mitalteli-family-book-chart\page.phtml
+    "To generate a pdf you can use print function and select &quot;Save as PDF&quot;, then specify a scale percentage in &quot;more options&quot;. With Chrome and Edge browsers on Windows OS, 47&percnt; has worked fine for me when showing 5 generations horizontaly and setting landscape US Letter page." => "Per a generar un PDF, pot utilitzar la funci&#xF3; d&#x27;impressi&#xF3; i seleccionar &#xAB;Guardar com PDF&#xBB;, i despr&#xE9;s especificar un percentatge d&#x27;escala en &#xAB;m&#xE9;s opcions&#xBB;. Amb els navegadors Chrome i *Edge en el sistema operatiu Windows, el 47% m&#x27;ha funcionat b&#xE9; en mostrar 5 generacions horitzontalment i configurar la p&#xE0;gina en format carta apa&#xEF;sat..", // resources\views\modules\mitalteli-family-book-chart\page.phtml
+    "print" => 'imprimir', // resources\views\modules\mitalteli-family-book-chart\page.phtml
+    "html" => 'html', // resources\views\modules\mitalteli-family-book-chart\page.phtml
+    "pdf" => 'pdf', // resources\views\modules\mitalteli-family-book-chart\page.phtml
+    "A3 (297mm x 420mm)" => 'A3 (297mm x 420mm)', // resources\views\modules\mitalteli-family-book-chart\page.phtml
+    "A4 (210mm x 297mm)" => 'A4 (210mm x 297mm)', // resources\views\modules\mitalteli-family-book-chart\page.phtml
+    "LETTER (8.5\" x 11\")" => 'Carta (8.5" x 11")', // resources\views\modules\mitalteli-family-book-chart\page.phtml
+    "LEGAL (8.5\" x 14\")" => 'Oficio (8.5" x 14")', // resources\views\modules\mitalteli-family-book-chart\page.phtml
+    "portrait" => 'vertical', // resources\views\modules\mitalteli-family-book-chart\page.phtml
+    "landscape" => 'horitzontal', // resources\views\modules\mitalteli-family-book-chart\page.phtml
+    "download" => 'descarregar', // resources\views\modules\mitalteli-family-book-chart\page.phtml
+    "Enhanced family book" => 'Llibre de família millorat', // FamilyBookChartEnhancedModule.php - function title()
+    "A chart of an individual’s ancestors and descendants, as a family book (enhanced with marriages, images, and more)." => 'Un quadre geneal&#xF2;gic dels avantpassats i descendents d&#x27;una persona, a mode de llibre de fam&#xED;lia (complementat amb matrimonis, imatges i molt m&#xE9;s)..', // FamilyBookChartEnhancedModule.php - function description()
+    "Enhanced family book of %s" => "Llibre de fam&#xED;lia millorat de %s", // FamilyBookChartEnhancedModule.php - function chartTitle()
+    "Full place name" => 'Lloc complet', // FamilyBookChartEnhancedModule.php
+    "City only" => "Nom&#xE9;s la ciutat",   // FamilyBookChartEnhancedModule.php
+    "City and country" => "Ciutat i pa&#xED;s",   // FamilyBookChartEnhancedModule.php
+    "City and 2 letter ISO country code" => "Ciutat i codi ISO de pa&#xED;s de 2 lletres",  // FamilyBookChartEnhancedModule.php
+    "City and 3 letter ISO country code" => "Ciutat i codi ISO de pa&#xED;s de 3 lletres", // FamilyBookChartEnhancedModule.php
+
+    ];
+?>
