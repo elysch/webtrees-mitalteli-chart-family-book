ba98f7e81ce1a55022cb597d0817e1c781a91953
elysch <ely.github.1@mitalteli.com>
2026-05-26 14:30:44 -0600

    Add Slovenian translation according to issue #13  (sl.php)

---
+++ resources/lang/sl.php
@@ -0,0 +1,45 @@
+<?php
+
+    return [
+
+    "Links" => 'Povezave', // resources\views\chart-box.phtml.php and resources\views\chart-box-marriage.phtml
+    "Family with spouse" => 'Družina s partnerjem', // resources\views\chart-box.phtml.php
+    "Family of %s" => 'Družina od %s', // resources\views\modules\mitalteli-family-book-chart\chart.phtml
+    #"Individual" => 'Oseba', // resources\views\modules\mitalteli-family-book-chart\page.phtml - Already included in webtrees translations
+    "Generations" => 'Generacije', // resources\views\modules\mitalteli-family-book-chart\page.phtml
+    "Descendant generations" => 'Naslednje generacije', // resources\views\modules\mitalteli-family-book-chart\page.phtml
+    "Places format" => 'Oblika za kraje', // resources\views\modules\mitalteli-family-book-chart\page.phtml
+    #"Spouses" => 'Partnerji', // resources\views\modules\mitalteli-family-book-chart\page.phtml - Already included in webtrees translations
+    "Show spouses" => 'Prikaži partnerje', // resources\views\modules\mitalteli-family-book-chart\page.phtml
+    #"Marriages" => 'Poroke', // resources\views\modules\mitalteli-family-book-chart\page.phtml - Already included in webtrees translations
+    "Show marriages" => 'Prikaži poroke', // resources\views\modules\mitalteli-family-book-chart\page.phtml
+    "Divorces" => 'Ločitve', // resources\views\modules\mitalteli-family-book-chart\page.phtml
+    "Show divorces" => 'Prikaži ločitve', // resources\views\modules\mitalteli-family-book-chart\page.phtml
+    "Extra images" => 'Dodatne slike', // resources\views\modules\mitalteli-family-book-chart\page.phtml
+    "Show extra images" => 'Prikaži dodatne slike', // resources\views\modules\mitalteli-family-book-chart\page.phtml
+    'Debug level' => 'Stopnja debug', // modules/mitalteli-family-book-chart/page.phtml
+    'No debug' => 'Brez debug', // modules/mitalteli-family-book-chart/page.phtml
+    'Place substitution debug' => 'Zamenjava krajev debug', // modules/mitalteli-family-book-chart/page.phtml
+    "view" => 'prikaži', // resources\views\modules\mitalteli-family-book-chart\page.phtml
+    "To generate a pdf you can use print function and select &quot;Save as PDF&quot;, then specify a scale percentage in &quot;more options&quot;. With Chrome and Edge browsers on Windows OS, 47&percnt; has worked fine for me when showing 5 generations horizontaly and setting landscape US Letter page." => 'Za izdelavo PDF-ja lahko uporabite funkcijo tiskanja in izberete &quot;Shrani kot PDF&quot;, nato pa pod &quot;več možnostmi&quot; določite odstotek povečave (scale). V brskalnikih Chrome in Edge v operacijskem sistemu Windows se je pri meni izkazalo, da 47&percnt; deluje dobro, če želite vodoravno prikazati 5 generacij na ležeče usmerjeni strani formata US Letter.', // resources\views\modules\mitalteli-family-book-chart\page.phtml
+    "print" => 'natisni', // resources\views\modules\mitalteli-family-book-chart\page.phtml
+    "html" => 'html', // resources\views\modules\mitalteli-family-book-chart\page.phtml
+    "pdf" => 'pdf', // resources\views\modules\mitalteli-family-book-chart\page.phtml
+    "A3 (297mm x 420mm)" => 'A3 (297mm x 420mm)', // resources\views\modules\mitalteli-family-book-chart\page.phtml
+    "A4 (210mm x 297mm)" => 'A4 (210mm x 297mm)', // resources\views\modules\mitalteli-family-book-chart\page.phtml
+    "LETTER (8.5\" x 11\")" => 'Letter (8.5" x 11")', // resources\views\modules\mitalteli-family-book-chart\page.phtml
+    "LEGAL (8.5\" x 14\")" => 'Legal (8.5" x 14")', // resources\views\modules\mitalteli-family-book-chart\page.phtml
+    "portrait" => 'pokončno', // resources\views\modules\mitalteli-family-book-chart\page.phtml
+    "landscape" => 'ležeče', // resources\views\modules\mitalteli-family-book-chart\page.phtml
+    "download" => 'prenesi', // resources\views\modules\mitalteli-family-book-chart\page.phtml
+    "Enhanced family book" => 'Družinska knjiga (izboljšana)', // FamilyBookChartEnhancedModule.php - function title()
+    "A chart of an individual’s ancestors and descendants, as a family book (enhanced with marriages, images, and more)." => 'Prikaz prednikov in potomcev posameznika v obliki družinske knjige (obogaten s porokami, slikami in drugimi podatki)."', // FamilyBookChartEnhancedModule.php - function description()
+    "Enhanced family book of %s" => 'Družinska knjiga za %s', // FamilyBookChartEnhancedModule.php - function chartTitle()
+    "Full place name" => 'Polni naziv kraja', // FamilyBookChartEnhancedModule.php
+    "City only" => "samo kraj",   // FamilyBookChartEnhancedModule.php
+    "City and country" => "kraj in država",   // FamilyBookChartEnhancedModule.php
+    "City and 2 letter ISO country code" => "Kraj in dvočrkovna ISO koda države",  // FamilyBookChartEnhancedModule.php
+    "City and 3 letter ISO country code" => "Kraj in tročrkovna ISO koda države", // FamilyBookChartEnhancedModule.php
+
+    ];
+?>
